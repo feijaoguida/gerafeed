f3ef0721d64bc84aa60ad712692b2e128fda1270
Roseweltty <feijaoguida@hotmail.com>
2026-08-30 15:49:24 -0300

    feat: refactor GoogleTagManager to support concurrent GTM container and GA4 tag injection

--- src/components/analytics/google-tag-manager.tsx
+++ src/components/analytics/google-tag-manager.tsx
@@ -5,19 +5,19 @@ import { useEffect } from "react";
 import { getStoredConsent, updateGtmConsent } from "@/lib/consent";
 
 interface GoogleTagManagerProps {
-  tagId?: string;
   gtmId?: string;
+  tagId?: string;
 }
 
 /**
- * Provedor do Google Tag (gtag.js) e Google Tag Manager (GTM).
+ * Provedor do Google Tag Manager (GTM) e Google Tag (gtag.js).
  * - Totalmente integrado com Google Consent Mode v2 (LGPD / ePrivacy).
  * - Suporta identificadores:
- *   - Google Analytics 4 / Google Tag: G-XXXXXX, GT-XXXXXX, AW-XXXXXX
- *   - Google Tag Manager Container: GTM-XXXXXX
+ *   - Google Tag Manager Container: GTM-MK74DPB7 (com suporte a noscript iframe)
+ *   - Google Analytics 4 / Google Tag: G-HDXPFN8TR0
  * - Injetado no RootLayout, cobrindo a home e todas as demais páginas públicas.
  */
-export function GoogleTagManager({ tagId, gtmId }: GoogleTagManagerProps) {
+export function GoogleTagManager({ gtmId, tagId }: GoogleTagManagerProps) {
   useEffect(() => {
     // Sincroniza consentimento prévio armazenado no client
     const consent = getStoredConsent();
@@ -26,18 +26,19 @@ export function GoogleTagManager({ tagId, gtmId }: GoogleTagManagerProps) {
     }
   }, []);
 
-  const activeId =
-    tagId ||
+  const activeGtmId =
     gtmId ||
+    process.env.NEXT_PUBLIC_GTM_ID ||
+    "GTM-MK74DPB7";
+
+  const activeTagId =
+    tagId ||
     process.env.NEXT_PUBLIC_GOOGLE_TAG_ID ||
     process.env.NEXT_PUBLIC_GA_ID ||
-    process.env.NEXT_PUBLIC_GTM_ID ||
     "G-HDXPFN8TR0";
 
-  const isGtm = Boolean(activeId && /^GTM-[A-Z0-9]+$/i.test(activeId));
-  const isGtag = Boolean(activeId && /^(G|GT|AW)-[A-Z0-9]+$/i.test(activeId));
-
-  if (!isGtm && !isGtag) return null;
+  const hasValidGtm = Boolean(activeGtmId && /^GTM-[A-Z0-9]+$/i.test(activeGtmId));
+  const hasValidTag = Boolean(activeTagId && /^(G|GT|AW)-[A-Z0-9]+$/i.test(activeTagId));
 
   return (
     <>
@@ -58,13 +59,40 @@ export function GoogleTagManager({ tagId, gtmId }: GoogleTagManagerProps) {
         }}
       />
 
-      {/* 2. Google Tag (gtag.js) para GA4 (ex: G-HDXPFN8TR0) */}
-      {isGtag && (
+      {/* 2. Container Google Tag Manager (GTM-MK74DPB7) */}
+      {hasValidGtm && (
+        <>
+          <Script
+            id="gtm-container"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${activeGtmId}');
+              `,
+            }}
+          />
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${activeGtmId}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+        </>
+      )}
+
+      {/* 3. Google Tag (gtag.js) para GA4 (G-HDXPFN8TR0) */}
+      {hasValidTag && (
         <>
           <Script
             id="google-tag-script"
             strategy="afterInteractive"
-            src={`https://www.googletagmanager.com/gtag/js?id=${activeId}`}
+            src={`https://www.googletagmanager.com/gtag/js?id=${activeTagId}`}
           />
           <Script
             id="google-tag-init"
@@ -74,28 +102,11 @@ export function GoogleTagManager({ tagId, gtmId }: GoogleTagManagerProps) {
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', '${activeId}');
+                gtag('config', '${activeTagId}');
               `,
             }}
           />
         </>
-      )}
-
-      {/* 3. Container GTM (ex: GTM-XXXXX) */}
-      {isGtm && (
-        <Script
-          id="gtm-container"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${activeId}');
-            `,
-          }}
-        />
       )}
     </>
   );
